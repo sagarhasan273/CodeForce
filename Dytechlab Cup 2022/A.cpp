93ff8c5bf93863b0cc5d17b8ed522255d230d9d9
sagarhasan273 <sagarhasan273@gmail.com>
2022-10-11 15:19:53 +0600

    DytechLab Cup 2022

---
+++ Dytechlab Cup 2022/A.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+#include<bits/stdc++.h>
+
+using namespace std;
+void solve(){
+    int n, k; cin >> n >> k;
+    string str;cin >> str;
+
+    vector<char> ch_list;
+    for (auto c: str) ch_list.push_back(c);
+
+
+
+}
+
+int main(){
+    int t;cin >> t;
+    while (t--) solve();
+}
