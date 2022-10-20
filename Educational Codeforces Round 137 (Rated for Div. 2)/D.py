1639dccbf1af026b010d47a54d41c1504ffe9c15
sagarhasan273 <sagarhasan273@gmail.com>
2022-10-20 15:48:30 +0600

    D solved

--- Educational Codeforces Round 137 (Rated for Div. 2)/D.py
+++ Educational Codeforces Round 137 (Rated for Div. 2)/D.py
@@ -1,27 +1,11 @@
-
-
 n = int(input())
+a = int(input(), 2)
+temp = a
+mx = a|temp
+for i in range(7):
+    temp = temp >> 1
+    m = a|temp
+    if mx < m:
+        mx = m
 
-a = input()
-
-sub_string = []
-
-for i in range(n):
-    s = []
-    for j in range(i):
-        s.append(a[j])
-        sub_string.append(s)
-res = 0
-
-for ar in sub_string:
-    v = [i for i in a]
-    v.reverse()
-    c = 0
-    for e in ar[::-1]:
-        if e == '1':
-            v[c] = '1'
-        c += 1
-
-    res = max(res, int("".join(v[::-1]), 2))
-
-print(bin(res).replace('0b', ''))
+print(bin(mx).replace('0b', ''))
