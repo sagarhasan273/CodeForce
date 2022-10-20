65ff4f90a537b77808a9f0fdb63c77cf12e10418
sagarhasan273 <sagarhasan273@gmail.com>
2022-10-20 23:30:41 +0600

    C solved

--- Educational Codeforces Round 137 (Rated for Div. 2)/C.py
+++ Educational Codeforces Round 137 (Rated for Div. 2)/C.py
@@ -1,5 +1,22 @@
 for _ in range(int(input())):
     n = int(input())
-    string = [int(i) for i in input()]
-    arr = list(map(int, input().split()))
+    string = '0' + input()
+    arr =  [0] + list(map(int, input().split()))
 
+    ans = 0
+    i = 0
+
+    while i <= n:
+        mn = arr[i]
+        sm = arr[i]
+        j = i + 1
+
+        while j <= n and string[j] == '1':
+            mn = min(mn, arr[j])
+            sm += arr[j]
+            j += 1
+        
+        ans += sm - mn
+        i = j
+    
+    print(ans)
