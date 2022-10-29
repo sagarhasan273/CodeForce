3c49a2988b816b148da2f03ebb3887a237c97ca7
sagarhasan273 <sagarhasan273@gmail.com>
2022-10-29 16:12:42 +0600

    A. Factorise N+M solved

--- Codeforces Round #831 (Div. 1 + Div. 2)/A.cpp
+++ Codeforces Round #831 (Div. 1 + Div. 2)/A.cpp
@@ -3,7 +3,12 @@
 using namespace std;
 
 void solve(){
-
+    int n; cin >> n;
+    if (n%2 == 0){
+        cout << 2 << endl;
+    }else{
+        cout << 3 << endl;
+    }
 }
 
 int main(){int t; cin >> t; while (t--) solve();}
