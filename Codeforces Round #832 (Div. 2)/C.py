4238f5ce81f20bbb995dbe292557f29fb3b0a8fd
sagarhasan273 <sagarhasan273@gmail.com>
2022-11-05 09:22:20 +0600

    C solved

--- Codeforces Round #832 (Div. 2)/C.py
+++ Codeforces Round #832 (Div. 2)/C.py
@@ -1,4 +1,9 @@
 for _ in range(int(input())):
     n = int(input())
     arr = list(map(int, input().split()))
-    
+    m = min(arr)
+
+    if m in arr[1:] and m != arr[0]:
+        print("Alice")
+    else:
+        print("Bob")
