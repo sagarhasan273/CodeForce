6cddc4e428a76354a895315ef6d4f7c0c1f6f987
sagarhasan273 <sagarhasan273@gmail.com>
2022-11-11 21:38:25 +0600

    B solved!

--- Codeforces Round #826 (Div. 3)/B.cpp
+++ Codeforces Round #826 (Div. 3)/B.cpp
@@ -3,7 +3,14 @@
 using namespace std;
 
 void solve(){
-    
+    int n; cin >> n;
+    if (n == 3) cout << -1 << endl;
+    else{
+        for (int i = 3; i <= n; i++){
+            cout << i << ' ';
+        }
+        cout << 2 << ' ' << 1 << endl;
+    }
 }
 
 int main(){
