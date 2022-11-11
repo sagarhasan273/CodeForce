63aa90af1c82b5e412994942770b55a1fdb26df1
sagarhasan273 <sagarhasan273@gmail.com>
2022-11-11 20:50:10 +0600

    B solved!

--- Codeforces Round #830 (Div. 2)/B.cpp
+++ Codeforces Round #830 (Div. 2)/B.cpp
@@ -3,7 +3,16 @@
 using namespace std;
 
 void solve(){
+    int n, ans = 0; cin >> n;
+    string s; cin >> s;
 
+    for (int i = 0; i < n-1; i ++){
+        if (s[i] != s[i+1]) ans++;
+    }
+
+    if (s[0] != '1' && ans >= 1) ans--;
+
+    cout << ans << endl;
 }
 
 int main(){
