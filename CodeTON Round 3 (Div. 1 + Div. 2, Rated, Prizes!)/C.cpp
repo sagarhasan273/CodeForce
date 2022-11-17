8f4e18e33098c4a6fd981869774d6fe3aa7eff6b
sagarhasan273 <sagarhasan273@gmail.com>
2022-11-17 17:29:20 +0600

    necessary code

--- CodeTON Round 3 (Div. 1 + Div. 2, Rated, Prizes!)/C.cpp
+++ CodeTON Round 3 (Div. 1 + Div. 2, Rated, Prizes!)/C.cpp
@@ -2,13 +2,20 @@
 
 using namespace std;
 
-typedef long long ll;
+#define int               long long
+#define f(i,a,b)          for(int i = a; i < b; i++)
+#define pb                push_back
+#define all(a)            a.begin(), a.end()
+#define arraysort(a, n)      sort(a, a + n)
+#define endl              "\n"
+#define inputarray(a, n)  f(i, 0, n) { cin >> a[i]; }
+#define printarray(a, n)  f(i, 0, n) { cout << a[i] << " "; }cout << endl;
 
 void testcase(){
-    
+    int n;
 }
 
-int main(){
+signed main(){
     int t; cin >> t;
     while (t--) testcase();
 }
