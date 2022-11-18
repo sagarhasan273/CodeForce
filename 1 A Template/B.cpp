de27ac330e0e6ea5544f9ab2e13b2ea6c526799f
sagarhasan273 <sagarhasan273@gmail.com>
2022-11-18 16:52:43 +0600

    Template changes

--- 1 A Template/B.cpp
+++ 1 A Template/B.cpp
@@ -3,11 +3,13 @@
 using namespace std;
 
 #define int                      long long
-#define for(i, a, n)             for (int i = a; i < n; i++)
-#define arrayinput(a, n)         for(i, 0, n) cin >> a[i];
-#define arrayprint(a, n)         for (i , 0, n) {cout << a[i] << ' ';} cout << endl;
+#define f(i, a, n)               for (int i = a; i < n; i++)
+#define arrayinput(a, n)         f(i, 0, n) cin >> a[i]
+#define arrayprint(a, n)         f(i , 0, n) {cout << a[i] << ' ';} cout << endl
 #define arraysort(a, n)          sort(a, a+n)
 #define all(a)                   a.begin(), a.end()
+#define print(a)                 {cout << a << endl; return;}
+#define vectorinput(v)           for(auto &i : v) cin >> i
 
 void testcase(){
 
--- Codeforces Round #828 (Div. 3)/D.cpp
+++ Codeforces Round #828 (Div. 3)/D.cpp
@@ -3,14 +3,46 @@
 using namespace std;
 
 #define int                      long long
-#define for(i, a, n)             for (int i = a; i < n; i++)
-#define arrayinput(a, n)         for(i, 0, n) cin >> a[i];
-#define arrayprint(a, n)         for (i , 0, n) {cout << a[i] << ' ';} cout << endl;
+#define f(i, a, n)               for (int i = a; i < n; i++)
+#define arrayinput(a, n)         f(i, 0, n) cin >> a[i]
+#define arrayprint(a, n)         f(i , 0, n) {cout << a[i] << ' ';} cout << endl
 #define arraysort(a, n)          sort(a, a+n)
 #define all(a)                   a.begin(), a.end()
+#define print(a)                 {cout << a << endl; return;}
+#define vectorinput(v)           for(auto &i : v) cin >> i
+
 
 void testcase(){
+    int n; cin >> n;
+    int arr[n], prod = 1;
+    arrayinput(arr, n);
 
+    f(i, 0, n) prod *= arr[i];
+
+    prod = prod % (int)(double)pow(2, n);
+
+    if (prod == 0){
+        cout << 0 << endl;
+        return;
+    }
+
+    int a[n], cnt = 0;
+    f(i, 0, n) a[i] = i+1;
+
+    for (int i = 0; i < n; i++){
+        if (prod % a[i] == 0){
+            prod = prod / 2;
+            cnt++;
+        }
+    }
+
+    if (prod == 1){
+        cout << cnt << endl;
+    }else{
+        cout << -1 << endl;
+    }
+
+    return;
 }
 
 signed main(){
