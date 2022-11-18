31f98ce8509eb2eaa8eafde06bea972c5a30d0a4
sagarhasan273 <sagarhasan273@gmail.com>
2022-11-18 19:25:50 +0600

    Template Edited!

--- 1 A Template/T.cpp
+++ 1 A Template/T.cpp
@@ -5,14 +5,19 @@ using namespace std;
 #define int                      long long
 #define f(i, a, n)               for (int i = a; i < n; i++)
 #define arrayinput(a, n)         f(i, 0, n) cin >> a[i]
-#define arrayprint(a, n)         f(i , 0, n) {cout << a[i] << ' ';} cout << endl
+#define arrayprint(a, n)         f(i, 0, n) {cout << a[i] << ' ';} cout << endl
 #define arraysort(a, n)          sort(a, a+n)
 #define all(a)                   a.begin(), a.end()
 #define print(a)                 {cout << a << endl; return;}
 #define vectorinput(v)           for(auto &i : v) cin >> i
+#define vectorprint(v)           for(auto x: v) {cout << x << ' ';} cout << endl
+
+
+// ---------------------Start Code Here---------------------
 
 void testcase(){
-
+    vector<int> vec(3, 0);
+    vectorprint(vec);
 }
 
 signed main(){
