4e24acb1c2c32a981461a48e9140915d2dba0298
sagarhasan273 <sagarhasan273@gmail.com>
2022-11-18 17:17:06 +0600

    D unsolved

--- Codeforces Round #828 (Div. 3)/D.cpp
+++ Codeforces Round #828 (Div. 3)/D.cpp
@@ -14,35 +14,39 @@ using namespace std;
 
 void testcase(){
     int n; cin >> n;
-    int arr[n], prod = 1;
-    arrayinput(arr, n);
+	int res = 0;
+	int count_2 = 0;
+	int num;
+	for (int i = 0; i < n; i++) {
+		cin >> num;
+		while (num % 2 == 0) {
+			count_2++;
+			num /= 2;
+		}
+	}
+	vector<int> num_2_i;
+    
+	for (int i = 2; i <= n; i += 2) {
+		int num_2 = 0;
+		int j = i;
+		while (j % 2 == 0) {
+			num_2++;
+			j /= 2;
+		}
+		num_2_i.push_back(num_2);
+	}
 
-    f(i, 0, n) prod *= arr[i];
+	sort(num_2_i.begin(), num_2_i.end());
 
-    prod = prod % (int)(double)pow(2, n);
+	int m = num_2_i.size() - 1;
 
-    if (prod == 0){
-        cout << 0 << endl;
-        return;
-    }
+	while (count_2 < n && m >= 0) {
+		count_2 += num_2_i[m--];
+		res++;
+	}
 
-    int a[n], cnt = 0;
-    f(i, 0, n) a[i] = i+1;
-
-    for (int i = 0; i < n; i++){
-        if (prod % a[i] == 0){
-            prod = prod / 2;
-            cnt++;
-        }
-    }
-
-    if (prod == 1){
-        cout << cnt << endl;
-    }else{
-        cout << -1 << endl;
-    }
-
-    return;
+	if (count_2 < n) print("-1")
+	else print(res);
 }
 
 signed main(){
