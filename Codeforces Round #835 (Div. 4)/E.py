7f38368ea5d1dd690cbdb53c2e1f414ffaa5bb62
sagarhasan273 <sagarhasan273@gmail.com>
2022-11-24 17:25:20 +0600

    E solved

--- Codeforces Round #835 (Div. 4)/E.py
+++ Codeforces Round #835 (Div. 4)/E.py
@@ -2,25 +2,4 @@ for _ in range(int(input())):
     n = int(input())
     arr = list(map(int, input().split()))
 
-    if arr[0] == 0:
-        arr[0] = 1
-
-        count = 0
-        for i in range(n):
-            if arr[i] == 1:
-                for j in range(i+1, n):
-                    if arr[j] == 0:
-                        count += 1
-        
-        print(count)
-    else:
-        
-
-        count = 0
-        for i in range(n):
-            if arr[i] == 1:
-                for j in range(i+1, n):
-                    if arr[j] == 0:
-                        count += 1
-
-        print(count)
+    
