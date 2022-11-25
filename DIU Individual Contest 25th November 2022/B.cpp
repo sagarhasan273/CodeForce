616648618022ec7ec6b1fe8173453d2966a4f3d2
sagarhasan273 <sagarhasan273@gmail.com>
2022-11-25 14:32:03 +0600

    B solved

--- DIU Individual Contest 25th November 2022/B.cpp
+++ DIU Individual Contest 25th November 2022/B.cpp
@@ -8,7 +8,7 @@ using namespace std;
 #define arrayprint(a, n)         f(i, 0, n) {cout << a[i] << ' ';} cout << endl
 #define arraysort(a, n)          sort(a, a+n)
 #define all(a)                   a.begin(), a.end()
-#define print(a)                 {cout << a << endl; return;}
+#define print(a)                 {cout << a;}
 #define vectorinput(v)           for(auto &i : v) cin >> i
 #define vectorprint(v)           for(auto x: v) {cout << x << ' ';} cout << endl
 
@@ -16,12 +16,34 @@ using namespace std;
 // ---------------------Start Code Here---------------------
 
 void testcase(){
-    
+    int n; cin >> n;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            if (i == n-1){
+                print('*')
+            }else{
+                if (j == 0 || j == n-1){
+                    print('*')
+                }else{
+                    print(' ')
+                }
+            }
+        }cout << ' ';
+
+        for(int j = 0; j < n; j++){
+            if (i == 0){
+                print('*')
+            }else{
+                if (j == 0 || j == n-1){
+                    print('*')
+                }else{
+                    print(' ')
+                }
+            }
+        }cout << endl;
+    }
 }
 
 signed main(){
-    int t; cin >> t; while (t--)
-    {
-        testcase();
-    }
+    testcase();
 }
