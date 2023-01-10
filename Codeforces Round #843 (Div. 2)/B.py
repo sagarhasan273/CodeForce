0b85c8236259af601a679441106285eee907e43d
sagarhasan273 <sagarhasan273@gmail.com>
2023-01-10 19:47:18 +0600

    A solved

--- Codeforces Round #843 (Div. 2)/B.py
+++ Codeforces Round #843 (Div. 2)/B.py
@@ -2,27 +2,14 @@ for _ in range(int(input())):
     s = input()
     flag = 0
 
-    if s.count('a') == 0:
-        print(s[0], s[1:-1], s[-1])
+    a = s[:1]
+    b = s[1:-1]
+    c = s[-1]
+    if (a <= b and c <= b):
+        print(a, b, c)
         flag = 1
-    elif s.count('a') == 1:
-        i = s.index('a')
-
-        if i == 0:
-            print(s[i], s[1:-1], s[-1])
-        elif i == len(s)-1:
-            print(s[0], s[1:-1], s[-1])
-        else:
-            print(s[:i], s[i], s[i+1:])
-        flag = 1
-    elif s.count('a') >= 2:
-        i = 0
-        while s[i] != 'a': i+=1
-
-        j = len(s)-1
-        while s[j] != 'a': j-=1
-
-        print(s[:i+1], s[i+1:j], s[j:])
+    elif a >= b and b <= c:
+        print(a, b, c)
         flag = 1
 
     if not flag:
